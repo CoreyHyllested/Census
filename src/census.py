de21dc1c4263c8c2e14951acf3149428cf80901c
Corey Hyllested <corey@herotime.co>
2015-09-07 14:54:20 -0600

    add Census.py, ScraperThread

---
+++ src/census.py
@@ -0,0 +1,88 @@
+import sys, os, threading
+import socks, socket, urllib3, requests
+import re, random, time
+import cmd, argparse
+import Queue
+from multiprocessing import Process
+from bs4 import BeautifulSoup, Comment
+from pprint		import pprint as pp
+from datetime	import datetime as dt 
+from models		import *
+
+
+VERSION = 0.01
+UA_VER	= 0.1
+THREADS	= 1
+
+sources = []
+threads	= []
+
+
+
+def configure_network(args):
+	host_response = requests.get('http://icanhazIP.com')
+
+	def create_connection(address, timeout=None, source_address=None):
+		sock = socks.socksocket()
+		sock.connect(address)
+		return sock
+
+	socks.setdefaultproxy(proxy_type=socks.PROXY_TYPE_SOCKS5, addr="127.0.0.1", port=9050)
+	socket.socket = socks.socksocket
+	socket.create_connection = create_connection
+
+	post_response = requests.get('http://icanhazIP.com')
+	print 'PrivacyCensus - Host IP: %s\t configure IP: %s ' % (host_response._content.rstrip(), post_response._content.rstrip())
+	if (host_response._content == post_response._content): print 'PrivacyCensus - Using host IP address.  Some sites may fail' #Not running tor:9050
+
+	# setup user-agent information
+	ua = None #urllib2.build_opener()
+	#ua = urllib2.build_opener()
+	#ua_string = 'PCBot/v%d' % UA_VER
+	#ua_string = 'Mozilla/5.0 (Linux; U; Android 4.0.4; en-gb; GT-I9300 Build/IMM76D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'
+	#ua.addheaders = [('User-agent', ua_string)]
+	return ua
+
+
+
+def load_sources(config_params):
+	sources = []
+	sources.append('https://www.google.com')
+
+	if (False): 
+		random.shuffle(sources, random.random)
+
+	q = Queue.Queue()
+	for website in sources:
+		q.put(website)
+
+	print 'PrivacyCensus - queue size %d' % (q.qsize())
+	return q
+
+
+
+
+
+
+if __name__ == '__main__':
+	print 'PrivacyCensus v' + str(VERSION)
+	parser = argparse.ArgumentParser(description='Scrape, normalize, and process html, javascript, and cookies')
+	parser.add_argument('-V', '--verbose',		action='store_true',	help='increase verbosity')
+	parser.add_argument('-O', '--overwrite',	action='store_true',	help='Overwrite output file')
+	parser.add_argument('-S', '--source',								help='Single source')
+	args = parser.parse_args()
+
+#	config_output_directories()
+	configure_network(args)
+
+	q = load_sources(args)
+	for thread_id in xrange(THREADS):
+		print 'PrivacyCensus - starting thread %d' % (thread_id)
+		t = ScraperThread(q, id=thread_id, debug=True)
+		t.start()
+		threads.append(t)
+
+	for thread in threads:
+		thread.join()
+
+	sys.exit()
---
+++ src/models/ScraperThread.py
@@ -0,0 +1,37 @@
+import threading, time
+import random
+import Queue
+from pprint		import pprint as pp
+from datetime	import datetime as dt 
+
+
+class ScraperThread(threading.Thread):
+	def __init__(self, q, id, debug=False):
+		threading.Thread.__init__(self)
+		self.q	= q
+		self.id	= id
+		self.debug	= debug
+		self.errors	= {}
+		self.doc_total	= q.qsize()
+		self.doc_count	= 0
+		self.doc_error	= 0
+		self.doc_dload	= 0
+
+
+	def run(self):
+		while not self.q.empty():
+			ts = dt.now()
+			document = self.q.get()
+			print document		#document.get_snapshot()
+			ts_diff = dt.now() - ts
+
+#			self.doc_count = self.doc_count + 1
+#			if (document.doc_state == 0x200):	self.doc_dload = self.doc_dload + 1
+#			if (document.doc_state & 0x400):
+#				self.doc_error = self.doc_error + 1
+#				self.errors[document.doc_state] = document.uri
+#			print 'Thread(%d): %d %d|%d|%d\t%s\t%s' % (self.id, self.doc_total, self.doc_count, self.doc_error, self.doc_dload, ts_diff, document.uri)
+		if (self.doc_error):
+			print 'Errors:'
+			pp(self.errors)
+
